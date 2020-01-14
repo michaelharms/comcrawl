6f61390f4d9aa472baa4aa89d1c10732d4be2761
Michael Harms <michaelharms.hsel@yahoo.de>
2020-01-14 08:26:11 +0100

    formatting

--- tests/comcrawl/utils/test_download_single_result.py
+++ tests/comcrawl/utils/test_download_single_result.py
@@ -8,13 +8,12 @@ KNOWN_RESULT = {
     'charset': 'UTF-8',
     'mime': 'text/html',
     'length': '3404',
-    'mime-detected':
-    'text/html',
+    'mime-detected': 'text/html',
     'offset': '68774745',
     'languages': 'eng',
     'digest': '745JGUNVPWB4L3TWJIGUQRQFTFSREJ5J',
-    'filename': 'crawl-data/CC-MAIN-2019-51/segments/1575540500637.40/warc/CC-MAIN-20191207160050-20191207184050-00394.warc.gz', 'status': '200'
-}
+    'filename': 'crawl-data/CC-MAIN-2019-51/segments/1575540500637.40/warc/CC-MAIN-20191207160050-20191207184050-00394.warc.gz',
+    'status': '200'}
 
 
 def test_download_single_result(snapshot):
