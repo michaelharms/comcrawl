f5dfbe4372e64eaaed1a7f039d738ab22a41a091
Michael Harms <michaelharms.hsel@yahoo.de>
2020-01-15 07:57:16 +0100

    doing some code style refactorings

--- comcrawl/api/search.py
+++ comcrawl/api/search.py
@@ -1,26 +1,28 @@
 from typing import List, Dict
-import concurrent.futures
+from concurrent import futures
 import pandas as pd
-from ..utils import search_single_index
+from ..utils import _search_single_index
 
 
-DEFAULT_INDEXES = open("comcrawl/config/default_indexes.txt", "r").read().split("\n")
+DEFAULT_INDEXES = (open("comcrawl/config/default_indexes.txt", "r")
+                   .read()
+                   .split("\n"))
 
 
-def search(
-        url: str,
-        indexes: List[str] = DEFAULT_INDEXES,
-        threads: int = None
-) -> List[Dict[str, Dict]]:
+def search(url: str,
+           indexes: List[str] = DEFAULT_INDEXES,
+           threads: int = None) -> List[Dict[str, Dict]]:
     """Searches multiple Common Crawl indices for URL pattern.
 
     Args:
         url: The URL pattern to search for.
         indices: List of Common Crawl indices to search in.
-        threads: Number of threads to use for faster search on multiple threads.
+        threads: Number of threads to use for faster search on
+        multiple threads.
 
     Returns:
-        List of all results found throughout the specified Common Crawl indices.
+        List of all results found throughout the specified
+        Common Crawl indices.
 
     """
 
@@ -28,22 +30,22 @@ def search(
 
     # multi-threaded search
     if threads:
-        with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+        with futures.ThreadPoolExecutor(max_workers=threads) as executor:
             future_to_index = {
                 executor.submit(
-                    search_single_index,
+                    _search_single_index,
                     index,
                     url
                 ): index for index in indexes
             }
 
-            for future in concurrent.futures.as_completed(future_to_index):
+            for future in futures.as_completed(future_to_index):
                 results.extend(future.result())
 
     # single-threaded search
     else:
         for index in indexes:
-            index_results = search_single_index(index, url)
+            index_results = _search_single_index(index, url)
             results.extend(index_results)
 
     return pd.DataFrame(results)
