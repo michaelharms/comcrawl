c385d3d79931345b927b65ebf6c3cc7b29596c9d
Michael Harms <michaelharms.hsel@yahoo.de>
2020-01-15 08:02:58 +0100

    fixing tests

--- tests/comcrawl/utils/test_search_single_index.py
+++ tests/comcrawl/utils/test_search_single_index.py
@@ -1,8 +1,8 @@
-from comcrawl.utils import search_single_index
+from comcrawl.utils import _search_single_index
 
 
 def test_search_single_index(snapshot):
-    results = search_single_index("2019-51", "https://index.commoncrawl.org/*")
+    results = _search_single_index("2019-51", "https://index.commoncrawl.org/*")
 
     assert len(results) == 3
     snapshot.assert_match(results)
