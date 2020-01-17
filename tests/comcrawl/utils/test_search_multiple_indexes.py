37f65f422c93206c42f61c8035c41e34e7a8de3f
Michael Harms <michaelharms.hsel@yahoo.de>
2020-01-17 22:08:08 +0100

    fixed flaky test

--- tests/comcrawl/utils/test_search_multiple_indexes.py
+++ tests/comcrawl/utils/test_search_multiple_indexes.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from comcrawl.utils import _search_multiple_indexes
 
 
@@ -10,4 +11,9 @@ def test_search_multiple_indexes_single_threaded(snapshot):
 def test_search_multiple_indexes_multi_threaded(snapshot):
     results = _search_multiple_indexes("https://index.commoncrawl.org", indexes=["2019-51", "2019-47"], threads=2)
 
-    snapshot.assert_match(results)
+    # sorting values to counteract the random results order, which is caused
+    # through the randomness in which thread search finished first
+    results_df = pd.DataFrame(results)
+    sorted_results_df = results_df.sort_values(by="timestamp")
+
+    snapshot.assert_match(sorted_results_df.to_dict("records"))
