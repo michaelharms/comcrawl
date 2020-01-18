55dee60bffbe219a91b2507313aad241bf3da103
Michael Harms <michaelharms.hsel@yahoo.de>
2020-01-18 16:39:21 +0100

    formatting

--- tests/comcrawl/utils/snapshots/snap_test_download.py
+++ tests/comcrawl/utils/snapshots/snap_test_download.py
@@ -721,12 +721,8 @@ Powered by <a href="https://github.com/webrecorder/pywb">pywb</a>
 </body>
 </html>'''
 
-snapshots['test_download_multiple_results_single_threaded 1'] = [
-    {
-        'charset': 'UTF-8',
-        'digest': '745JGUNVPWB4L3TWJIGUQRQFTFSREJ5J',
-        'filename': 'crawl-data/CC-MAIN-2019-51/segments/1575540500637.40/warc/CC-MAIN-20191207160050-20191207184050-00394.warc.gz',
-        'html': '''<!DOCTYPE html>
+snapshots['test_download_multiple_results_single_threaded 1'] = [{
+    'charset': 'UTF-8', 'digest': '745JGUNVPWB4L3TWJIGUQRQFTFSREJ5J', 'filename': 'crawl-data/CC-MAIN-2019-51/segments/1575540500637.40/warc/CC-MAIN-20191207160050-20191207184050-00394.warc.gz', 'html': '''<!DOCTYPE html>
 <html>
 <head>
 <link rel="stylesheet" href="/static/__shared/shared.css"/>
@@ -1438,22 +1434,7 @@ Powered by <a href="https://github.com/webrecorder/pywb">pywb</a>
 </p>
 
 </body>
-</html>''',
-        'languages': 'eng',
-        'length': '3404',
-        'mime': 'text/html',
-        'mime-detected': 'text/html',
-        'offset': '68774745',
-        'status': '200',
-        'timestamp': '20191207172145',
-        'url': 'http://index.commoncrawl.org/',
-        'urlkey': 'org,commoncrawl,index)/'
-    },
-    {
-        'charset': 'UTF-8',
-        'digest': 'SVH4V5QDUS7SMXSXZYB2XWJSVDWFXUD7',
-        'filename': 'crawl-data/CC-MAIN-2019-47/segments/1573496667767.6/warc/CC-MAIN-20191114002636-20191114030636-00394.warc.gz',
-        'html': '''<!DOCTYPE html>
+</html>''', 'languages': 'eng', 'length': '3404', 'mime': 'text/html', 'mime-detected': 'text/html', 'offset': '68774745', 'status': '200', 'timestamp': '20191207172145', 'url': 'http://index.commoncrawl.org/', 'urlkey': 'org,commoncrawl,index)/'}, {'charset': 'UTF-8', 'digest': 'SVH4V5QDUS7SMXSXZYB2XWJSVDWFXUD7', 'filename': 'crawl-data/CC-MAIN-2019-47/segments/1573496667767.6/warc/CC-MAIN-20191114002636-20191114030636-00394.warc.gz', 'html': '''<!DOCTYPE html>
 <html>
 <head>
 <link rel="stylesheet" href="/static/__shared/shared.css"/>
@@ -2155,18 +2136,7 @@ Powered by <a href="https://github.com/webrecorder/pywb">pywb</a>
 </p>
 
 </body>
-</html>''',
-        'languages': 'eng',
-        'length': '3391',
-        'mime': 'text/html',
-        'mime-detected': 'text/html',
-        'offset': '82652447',
-        'status': '200',
-        'timestamp': '20191114010130',
-        'url': 'http://index.commoncrawl.org/',
-        'urlkey': 'org,commoncrawl,index)/'
-    }
-]
+</html>''', 'languages': 'eng', 'length': '3391', 'mime': 'text/html', 'mime-detected': 'text/html', 'offset': '82652447', 'status': '200', 'timestamp': '20191114010130', 'url': 'http://index.commoncrawl.org/', 'urlkey': 'org,commoncrawl,index)/'}]
 
 snapshots['test_download_multiple_results_multi_threaded 1'] = [
     {
